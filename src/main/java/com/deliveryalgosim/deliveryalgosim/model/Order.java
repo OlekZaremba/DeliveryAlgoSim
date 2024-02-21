b1002765aecc1c6f986589e8bba05913d83966ce
OlekZaremba <sarutern.gra@gmail.com>
2024-02-21 20:32:30 +0100

    Initial commit

--- src/main/java/com/deliveryalgosim/deliveryalgosim/model/Order.java
+++ src/main/java/com/deliveryalgosim/deliveryalgosim/model/Order.java
@@ -24,17 +24,31 @@ public class Order extends Grid {
 
         String[] solids = {"Chicken Burger", "Cheeseburger","Chicken nuggets", "Chicken wings", "Chicken tenders",
                 "Chicken thighs", "Chicken quesadilla"};
-        String[] drinks = {"Fies", "Cola", "Fanta", "Sprite", "Ice tea lemon", "Ice tea peach"};
+        String[] drinks = {"Fries", "Cola", "Fanta", "Sprite", "Ice tea lemon", "Ice tea peach"};
         Random random = new Random();
         StringBuilder solidsOrdered = new StringBuilder();
         int numberOfSolids = random.nextInt(1,5);
-        for (int i = 1; i<=numberOfSolids; i++){
-            int solidsId = random.nextInt(1, 7);
+        for (int i = 0; i<=numberOfSolids; i++){
+            int solidsId = random.nextInt(solids.length);
             solidsOrdered.append(solids[solidsId]);
-
-
+            if (i < numberOfSolids - 1) {
+                solidsOrdered.append(", ");
+            }
         }
-        return null;
+
+        StringBuilder drinksOrdered = new StringBuilder();
+        int numberOfDrinks = random.nextInt(1,3);
+        for (int i = 1; i<=numberOfDrinks; i++){
+            int drinksId = random.nextInt(drinks.length);
+            drinksOrdered.append(drinks[drinksId]);
+            if (i < numberOfDrinks - 1) {
+                drinksOrdered.append(", ");
+            }
+        }
+
+
+
+        return solidsOrdered + " with " + drinksOrdered;
     }
 
 
